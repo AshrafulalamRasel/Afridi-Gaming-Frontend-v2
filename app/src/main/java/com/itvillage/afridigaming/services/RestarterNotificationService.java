d454825edbedfcc1f2175e3438264962ce988ed4
Moniruzzaman Rony <eproni29@gmail.com>
2021-08-05 20:03:57 +0600

    App Update version 4

--- app/src/main/java/com/itvillage/afridigaming/services/RestarterNotificationService.java
+++ app/src/main/java/com/itvillage/afridigaming/services/RestarterNotificationService.java
@@ -39,9 +39,9 @@ public class RestarterNotificationService extends BroadcastReceiver {
         Toast.makeText(context.getApplicationContext(), "Notification Background Started",Toast.LENGTH_SHORT).show();
 
 
-        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-        Ringtone r = RingtoneManager.getRingtone(context, notification);
-        r.play();
+//        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+//        Ringtone r = RingtoneManager.getRingtone(context, notification);
+//        r.play();
 
         timer = new Timer();
         timerTask = new TimerTask() {
